e78afb10158a42bfc4bc5749b4247980b6c38138
jzalger <jzalger+github@jzalger.net>
2023-04-01 12:04:30 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include "sbus.h"
+
+#define RECORD_PIN 0
+#define AUX_UP 1
+#define AUX_DOWN 2
+
+#define SBUS_HIGH 212
+#define SBUS_MID 128
+#define SBUS_MIN 44
+
+bool rec_state = false;
+bool aux_up_state = false;
+bool aux_down_state = false;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RECORD_PIN, INPUT);
+  pinMode(AUX_UP, INPUT);
+  pinMode(AUX_DOWN, INPUT);
+}
+
+void loop() {
+  rec_state = digitalRead(RECORD_PIN);
+  aux_up_state = digitalRead(AUX_UP);
+  aux_down_state = digitalRead(AUX_DOWN);
+}
