f14a32ddf0a30a46cd92af1e72a186b6d6cf0435
jzalger <jzalger@users.noreply.github.com>
2023-04-03 21:26:10 -0400

    Fixed recording control

--- include/main.h
+++ include/main.h
@@ -6,9 +6,10 @@
 #define AUX_UP_PIN A1
 #define AUX_DOWN_PIN A5
 
-#define SBUS_HIGH 212
-#define SBUS_MID 128
-#define SBUS_MIN 44
+#define SBUS_HIGH 2046
+#define SBUS_MID 1024
+#define SBUS_MIN 0
+#define SBUS_DELAY 6
 #define SERIAL_RX_PIN 16
 #define SERIAL_TX_PIN 17
 
@@ -16,13 +17,17 @@
 #define SBUS_IRIS_CH 1
 #define SBUS_ISO_CH 2
 
+
+
 bool recording = false;
-uint8_t iris_index = 8;
+uint8_t iris_index = 0;
 
 struct Button {
   const uint8_t pin;
   bool pressed;
 };
+
+bfs::SbusData data;
 
 // Equates to [f1.8, f2, f2.8, f4, f5.6, f8, f11, f16, f22]
 uint8_t apertures[9] = {44, 65, 90, 110, 120, 140, 155, 180, 212};
@@ -31,6 +36,8 @@ void IRAM_ATTR record_isr();
 void IRAM_ATTR aux_up_isr();
 void IRAM_ATTR aux_down_isr();
 
+void toggle_sbus();
+void init_sbus();
 void toggle_record();
 void increase_fstop();
 void decrease_fstop();
--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ unsigned long last_button_time = 0;
 
 void IRAM_ATTR record_isr(){
   button_time = millis();
-  if (button_time - last_button_time > 500){
+  if (button_time - last_button_time > 300){
     record_btn.pressed = true;
     last_button_time = button_time;
   }
@@ -20,7 +20,7 @@ void IRAM_ATTR record_isr(){
 
 void IRAM_ATTR aux_up_isr(){
   button_time = millis();
-  if (button_time - last_button_time > 500){
+  if (button_time - last_button_time > 300){
     aux_up_btn.pressed = true;
     last_button_time = button_time;
   }
@@ -28,7 +28,7 @@ void IRAM_ATTR aux_up_isr(){
 
 void IRAM_ATTR aux_down_isr(){
   button_time = millis();
-  if (button_time - last_button_time > 500){
+  if (button_time - last_button_time > 300){
     aux_down_btn.pressed = true;
     last_button_time = button_time;
   }
@@ -50,17 +50,14 @@ void setup() {
 void loop() {
   if (record_btn.pressed) {
     toggle_record();
-    Serial.printf("Toggle Record\n");
     record_btn.pressed = false;
   }
   if (aux_up_btn.pressed) {
     increase_fstop();
-    Serial.printf("Increase fstop\n");
     aux_up_btn.pressed = false;
   }
   if (aux_down_btn.pressed) {
     decrease_fstop();
-    Serial.printf("Decrease fstop\n");
     aux_down_btn.pressed = false;
   }
 }
@@ -69,18 +66,29 @@ void loop() {
 
 void send_sbus(int ch, int value) {
   //Writes the value for a specific channel to the SBUS
-  bfs::SbusData data;
   data.ch[ch] = value;
   sbus.data(data);
   sbus.Write();
 }
 
+void toggle_sbus(int ch, int value){
+    send_sbus(ch, SBUS_MID);
+    vTaskDelay(SBUS_DELAY);
+    send_sbus(ch, value);
+    vTaskDelay(SBUS_DELAY);
+    send_sbus(ch, SBUS_MID);
+}
+
 void toggle_record() {
   // Starts or stops camera recording
   if (recording == false){
-    send_sbus(SBUS_REC_CH, SBUS_HIGH);
+    toggle_sbus(SBUS_REC_CH, SBUS_HIGH);
+    recording = true;
+    Serial.printf("Recording\n");
   } else {
-    send_sbus(SBUS_REC_CH, SBUS_MIN);
+    toggle_sbus(SBUS_REC_CH, SBUS_MIN);
+    recording = false;
+    Serial.printf("Stopped Recording\n");
   }
   
 }
@@ -89,8 +97,9 @@ void increase_fstop() {
   // Increases the f-stop (closes the aperature) by one stop
   if (iris_index < 8) {
     ++iris_index;
+    toggle_sbus(SBUS_IRIS_CH, SBUS_MIN);
     Serial.printf("Sending aperature val: %d at index %d\n", apertures[iris_index], iris_index);
-    send_sbus(SBUS_IRIS_CH, apertures[iris_index]);
+    //send_sbus(SBUS_IRIS_CH, apertures[iris_index]);
   }
 }
 
@@ -98,7 +107,22 @@ void decrease_fstop() {
   // Decreases the f-stop (opens the aperature) by one stop
     if (iris_index > 0) {
     --iris_index;
+    toggle_sbus(SBUS_IRIS_CH, SBUS_HIGH);
     Serial.printf("Sending aperature val: %d at index %d\n", apertures[iris_index], iris_index);
-    send_sbus(SBUS_IRIS_CH, apertures[iris_index]);
+    //send_sbus(SBUS_IRIS_CH, apertures[iris_index]);
   }
 }
+
+ void init_sbus(){
+  for (int i = 0;i < 16;++i){
+    data.ch[i] = SBUS_MID;
+  }
+  data.lost_frame = false;
+  data.failsafe = false;
+  data.ch17 = false;
+  data.ch18 = false;
+  data.ch[SBUS_REC_CH] = SBUS_MID;
+  data.ch[SBUS_IRIS_CH] = apertures[iris_index];
+  sbus.data(data);
+  sbus.Write();
+ }
