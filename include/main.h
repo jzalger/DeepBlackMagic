925124c6a95ba9f569f16dfef9cdfed17bb7f16e
jzalger <jzalger@users.noreply.github.com>
2023-04-03 22:00:42 -0400

    Refactoring

--- include/main.h
+++ include/main.h
@@ -16,13 +16,9 @@
 
 #define SBUS_REC_CH 0
 #define SBUS_IRIS_CH 1
-#define SBUS_ISO_CH 2
-
-
 
 bool recording = false;
 uint8_t iris_index = 0;
-uint16_t iris_level = 1748;
 
 struct Button {
   const uint8_t pin;
@@ -32,7 +28,6 @@ struct Button {
 bfs::SbusData data;
 
 // Equates to [f1.8, f2, f2.8, f4, f5.6, f8, f11, f16, f22]
-//uint8_t apertures[9] = {44, 65, 90, 110, 120, 140, 155, 180, 212};
 uint16_t apertures[9] = {300, 460, 620, 780, 940, 1100, 1260, 1420, 1600};
 
 void IRAM_ATTR record_isr();
@@ -40,7 +35,6 @@ void IRAM_ATTR aux_up_isr();
 void IRAM_ATTR aux_down_isr();
 
 void toggle_sbus();
-void init_sbus();
 void toggle_record();
 void increase_fstop();
 void decrease_fstop();
--- src/main.cpp
+++ src/main.cpp
@@ -84,51 +84,26 @@ void toggle_record() {
   if (recording == false){
     toggle_sbus(SBUS_REC_CH, SBUS_HIGH);
     recording = true;
-    Serial.printf("Recording\n");
   } else {
     toggle_sbus(SBUS_REC_CH, SBUS_MIN);
     recording = false;
-    Serial.printf("Stopped Recording\n");
   }
-  
 }
 
 void increase_fstop() {
   // Increases the f-stop (closes the aperature) by one stop
   if (iris_index < 8) {
     ++iris_index;
-    //toggle_sbus(SBUS_IRIS_CH, SBUS_MIN);
-    Serial.printf("Sending aperature val: %d at index %d\n", apertures[iris_index], iris_index);
     send_sbus(SBUS_IRIS_CH, apertures[iris_index]);
     vTaskDelay(SBUS_DELAY);
   }
-  // iris_level = iris_index + SBUS_INC;
-  // send_sbus(SBUS_IRIS_CH, iris_level);
 }
 
 void decrease_fstop() {
   // Decreases the f-stop (opens the aperature) by one stop
     if (iris_index > 0) {
     --iris_index;
-    //toggle_sbus(SBUS_IRIS_CH, SBUS_HIGH);
-    Serial.printf("Sending aperature val: %d at index %d\n", apertures[iris_index], iris_index);
     send_sbus(SBUS_IRIS_CH, apertures[iris_index]);
     vTaskDelay(SBUS_DELAY);
   }
-  // iris_level = iris_index - SBUS_INC;
-  // send_sbus(SBUS_IRIS_CH, iris_level);
 }
-
- void init_sbus(){
-  for (int i = 0;i < 16;++i){
-    data.ch[i] = SBUS_MID;
-  }
-  data.lost_frame = false;
-  data.failsafe = false;
-  data.ch17 = false;
-  data.ch18 = false;
-  data.ch[SBUS_REC_CH] = SBUS_MID;
-  data.ch[SBUS_IRIS_CH] = apertures[iris_index];
-  sbus.data(data);
-  sbus.Write();
- }
